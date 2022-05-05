b0ab4ce87e1c648a1efc3a5a7f8c9c5346ef4334
Martin Duttenhöfner <martin.duttenhoefner@hs-furtwangen.de>
2022-05-05 22:01:26 +0200

    JavaDoc hinzugefügt

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -6,16 +6,23 @@ package de.hfu;
  */
 public class App 
 {
+	/**
+	 * This Program replaces every lowercase characters in a string
+	 *
+	 */
     public static void main(String[] args) {
 
+    	/**
+    	 * Here we declare a potential string
+    	 *
+    	 */
         String str1="Das ist ein Teststring";
         StringBuffer newStr=new StringBuffer(str1);
 
         for(int i = 0; i < str1.length(); i++) {
-
-            //Checks for lower case character
+        	
+        	
             if(Character.isLowerCase(str1.charAt(i))) {
-                //Convert it into upper case using toUpperCase() function
                 newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));
             }
 
